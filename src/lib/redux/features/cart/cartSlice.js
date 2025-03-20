4173c87d94dd173068efd5dc295e5fd16fe80365
AfrideHossain <afredehossain@gmail.com>
2025-03-20 22:54:52 +0600

    Enhance cart slice with additional reducers for item removal, update, and clearing cart state; add comments for clarity

--- src/lib/redux/features/cart/cartSlice.js
+++ src/lib/redux/features/cart/cartSlice.js
@@ -9,9 +9,11 @@ export const cartSlice = createSlice({
   initialState,
   reducers: {
     setCart: (state, action) => {
+      // set cart state
       state.items = action.payload;
     },
     add: (state, { payload }) => {
+      // Add a item in cart state
       const existingIndex = state.items.findIndex(
         (item) => item.productId === payload.productId
       );
@@ -24,15 +26,18 @@ export const cartSlice = createSlice({
       }
     },
     clear: (state) => {
+      // clear entire cart state
       state.items = [];
     },
     removeItem: (state, { payload }) => {
+      //remove an item from cart state
       const newItems = state.items.filter(
         (item) => item.productId !== payload.productId
       );
       state.items = newItems;
     },
     updateItem: (state, { payload }) => {
+      //update an existing cart item
       const existingIndex = state.items.findIndex(
         (item) => item.productId === payload.productId
       );
